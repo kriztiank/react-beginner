f08cca8129f6225e2cda2ed911c34a5df46ef0ac
kriztiank <kriztiank@gmail.com>
2023-02-02 09:58:46 +0100

    delete & complete tasks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css'
 import { useState } from 'react'
-// import { Task } from "./Task";
+import { Task } from './Task'
 
 function App() {
   const [todoList, setTodoList] = useState([])
@@ -11,7 +11,29 @@ function App() {
   }
 
   const addTask = () => {
-    setTodoList([...todoList, newTask])
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+      completed: false,
+    }
+
+    setTodoList([...todoList, task])
+  }
+
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter((task) => task.id !== id))
+  }
+
+  const completeTask = (id) => {
+    setTodoList(
+      todoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: true }
+        } else {
+          return task
+        }
+      })
+    )
   }
 
   return (
@@ -22,7 +44,15 @@ function App() {
       </div>
       <div className='list'>
         {todoList.map((task) => {
-          return <h1>{task}</h1>
+          return (
+            <Task
+              taskName={task.taskName}
+              id={task.id}
+              completed={task.completed}
+              deleteTask={deleteTask}
+              completeTask={completeTask}
+            />
+          )
         })}
       </div>
     </div>
